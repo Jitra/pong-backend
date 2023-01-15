b4ac00c7b70eff35e1b1e64dc2e0ad3245fbdd84
MK <wozniak.mikolaj@outlook.com>
2023-01-15 08:56:14 +0000

    Update 3 files

--- src/gateway/pong.gateway.ts
+++ src/gateway/pong.gateway.ts
@@ -6,7 +6,7 @@ import {
      MessageBody 
 } from '@nestjs/websockets/decorators';
 import { OnModuleInit } from '@nestjs/common';
-
+import { Pong } from './types';
 import { Server, Socket } from 'socket.io';
 
 @WebSocketGateway({
@@ -17,26 +17,38 @@ import { Server, Socket } from 'socket.io';
 export class PongGateway implements OnModuleInit {
     
     players: {id: string, name: string}[] = [];
-    
+    pong: Pong;
     @WebSocketServer()
     server: Server;
     
     onModuleInit(){
-        console.log('init');
-        this.server.on('connection', () => {
-            console.log('new player connected');
+        
+        this.server.on('connection', (socket: Socket) => { 
             this.server.emit('pong.members', this.players);
+            socket.on('disconnecting', () => {
+                this.players = this.players.filter(p => p.id !== socket.id);
+                this.server.emit('pong.members', this.players);
+                if(this.players.length < 2){
+                    this.pong = null;
+                }
+            })
         })
-        this.server.on('disconnecting', (socket: Socket) => {
-            console.log('disconnecting', socket.id);
-            this.players = this.players.filter(p => p.id !== socket.id);
-            this.server.emit('pong.members', this.players);
-        })
+        
+        setInterval(() => {
+            if(this.players.length >= 2 && !this.pong){
+                this.pong = new Pong(600, 400, this.players[0], this.players[1]);
+            }
+            if(this.pong){
+                this.pong.update();
+                this.server.emit('pong.cast', this.pong.show())
+            }
+        }, 15)
     }
     
     @SubscribeMessage('chat-message')
     chat(@ConnectedSocket() socket: Socket, @MessageBody() data: any){
-        socket.broadcast.emit('chat-message', data);
+        const name = this.players.find(p => p.id === socket.id)?.name;
+        socket.broadcast.emit('chat-message', {message: data, name: name ?? 'Anonymouns'});
         console.log(`New message from ${socket.id}`, data);
     }
     
@@ -44,5 +56,28 @@ export class PongGateway implements OnModuleInit {
     pongJoinMember(@ConnectedSocket() socket: Socket, @MessageBody() name: string){
         this.players.push({id: socket.id, name});
         this.server.emit('pong.members', this.players);
+    }
+    
+    
+    @SubscribeMessage('pong.move')
+    pongMove(@ConnectedSocket() socket: Socket, @MessageBody() key: string){
+        const player = this.pong && this.pong.player(socket.id);
+        if(!player){
+            return;
+        }
+       switch(key){
+           case 'ArrowUp': {
+               player.move(-10);
+               break;
+           }
+           case 'ArrowDown' : {
+               player.move(10);
+               break;
+           }
+           default: {
+               player.move(0);
+               break;
+           }
+       }
     }
 }
